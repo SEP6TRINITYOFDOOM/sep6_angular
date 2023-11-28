46926d0e4f99f70e6bb44badd5b9275849b4c2e9
Esben Fogh <esbenvensel.fogh@gmail.com>
2023-11-28 17:42:44 +0100

    Added html for registration page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, RegisterComponent]
 })
 export class AppModule { }
