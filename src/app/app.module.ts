8bbbdd8e520308e785d8f93cbf05b6cb4ee1ca79
Esben Fogh <esbenvensel.fogh@gmail.com>
2023-11-28 16:26:14 +0100

    HTML for Homepage
    Html with boostrap for the homepage. This includes two components such as navigation and carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MovieCarouselComponent } from './movie-carousel/movie-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavigationComponent,
+    MovieCarouselComponent
   ],
   imports: [
     BrowserModule
