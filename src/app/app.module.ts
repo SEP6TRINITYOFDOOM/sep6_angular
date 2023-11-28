504652538f056c45287af7a76e0c053ae0bf4fc2
Adam Arasimowicz <75214235+arashimoya@users.noreply.github.com>
2023-11-28 23:17:46 +0100

    account view done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { NavigationComponent } from './homepage/Components/navigation/navigation.component';
-import { MovieCarouselComponent } from './homepage/Components/movie-carousel/movie-carousel.component';
-import { LoginComponent } from './views/login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {RegisterComponent} from './register/register.component';
+import {HomepageComponent} from './homepage/homepage.component';
+import {NavigationComponent} from './homepage/Components/navigation/navigation.component';
+import {MovieCarouselComponent} from './homepage/Components/movie-carousel/movie-carousel.component';
+import {LoginComponent} from './views/login/login.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
 import {MatIconModule} from "@angular/material/icon";
-import { ProfileComponent } from './views/profile/profile.component';
+import {ProfileComponent} from './views/profile/profile.component';
 import {MatTabsModule} from "@angular/material/tabs";
+import {MovieItemComponent} from './views/movie-item/movie-item.component';
+import {MatCardModule} from "@angular/material/card";
+import {CommonModule} from "@angular/common";
+import {FriendItemComponent} from './views/friend-item/friend-item.component';
+import {AccountSettingsComponent} from './views/account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +28,9 @@ import {MatTabsModule} from "@angular/material/tabs";
     MovieCarouselComponent,
     LoginComponent,
     ProfileComponent,
+    MovieItemComponent,
+    FriendItemComponent,
+    AccountSettingsComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,9 +38,12 @@ import {MatTabsModule} from "@angular/material/tabs";
     MatButtonModule,
     MatInputModule,
     MatIconModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule,
+    CommonModule
   ],
   providers: [],
-  bootstrap: [AppComponent,HomepageComponent, NavigationComponent, MovieCarouselComponent, RegisterComponent]
+  bootstrap: [AppComponent, HomepageComponent, NavigationComponent, MovieCarouselComponent, RegisterComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
