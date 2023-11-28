340c72d6246bc96473619c51d1e65c8973a855f1
Adam Arasimowicz <75214235+arashimoya@users.noreply.github.com>
2023-11-28 23:17:46 +0100

    account view done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { NavigationComponent } from './homepage/Components/navigation/navigation.component';
-import { MovieCarouselComponent } from './homepage/Components/movie-carousel/movie-carousel.component';
+import {AppComponent} from './app.component';
+import {RegisterComponent} from './register/register.component';
+import {HomepageComponent} from './homepage/homepage.component';
+import {NavigationComponent} from './homepage/Components/navigation/navigation.component';
+import {MovieCarouselComponent} from './homepage/Components/movie-carousel/movie-carousel.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {ProfileComponent} from './views/profile/profile.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import {MovieItemComponent} from './views/movie-item/movie-item.component';
+import {MatCardModule} from "@angular/material/card";
+import {CommonModule} from "@angular/common";
+import {FriendItemComponent} from './views/friend-item/friend-item.component';
+import {AccountSettingsComponent} from './views/account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
@@ -13,12 +24,24 @@ import { MovieCarouselComponent } from './homepage/Components/movie-carousel/mov
     RegisterComponent,
     HomepageComponent,
     NavigationComponent,
-    MovieCarouselComponent
+    MovieCarouselComponent,
+    ProfileComponent,
+    MovieItemComponent,
+    FriendItemComponent,
+    AccountSettingsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatTabsModule,
+    MatCardModule,
+    CommonModule
   ],
   providers: [],
-  bootstrap: [AppComponent,HomepageComponent, NavigationComponent, MovieCarouselComponent, RegisterComponent]
+  bootstrap: [AppComponent, HomepageComponent, NavigationComponent, MovieCarouselComponent, RegisterComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/views/profile/profile.component.ts
@@ -0,0 +1,71 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent {
+  movies: Movie[] = []
+  friends: Friend[] = []
+
+  constructor() {
+    this.movies = [
+      {
+        title: 'Mulholland Drive',
+        posterUrl: "https://xl.movieposterdb.com/07_10/2001/166924/xl_166924_22c04427.jpg?v=2023-09-14%2018:20:37"
+      },
+      {
+        title: 'Napoleon',
+        posterUrl: "https://xl.movieposterdb.com/23_10/0/13287846/xl_napoleon-movie-poster_38357dbe.jpg?v=2023-11-25%2019:09:33"
+      },
+      {
+        title: 'Bladerunner 2049',
+        posterUrl: "https://xl.movieposterdb.com/19_12/2017/1856101/xl_1856101_2c68bb27.jpg?v=2023-11-12%2017:50:17"
+      },
+      {
+        title: 'Nightcrawler',
+        posterUrl: "https://posters.movieposterdb.com/15_01/2014/2872718/l_2872718_8fbb66d3.jpg"
+      },
+      {
+        title: 'Ford v Ferrari',
+        posterUrl: "https://posters.movieposterdb.com/20_03/1995/114369/l_114369_ff13577f.jpg"
+      },
+      {
+        title: 'American Psycho',
+        posterUrl: "https://posters.movieposterdb.com/11_08/2000/144084/l_144084_1edb3ca0.jpg"
+      },
+      {
+        title: 'Druk',
+        posterUrl: "https://posters.movieposterdb.com/21_01/2020/10288566/l_10288566_43c6cd69.jpg"
+      },
+    ]
+
+    this.friends = [
+      this.friend('Alex Johnson'),
+      this.friend('Emily Rodriguez'),
+      this.friend('Marcus Williams'),
+      this.friend('Jasmine Lee'),
+      this.friend('Benjamin Taylor'),
+      this.friend('Olivia Brown'),
+      this.friend('Daniel Martinez'),
+    ]
+  }
+
+  private friend(name: string, url: string = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"): Friend {
+    return {name: name, profilePicUrl: url}
+  }
+
+
+}
+
+//TODO subject to change
+export interface Movie {
+  title: string,
+  posterUrl: string
+}
+
+export interface Friend {
+  name: string,
+  profilePicUrl: string
+}
