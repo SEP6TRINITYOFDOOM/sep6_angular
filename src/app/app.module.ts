bfe6a9a541943f8e8a6983f0c31918796875f076
Esben Fogh <esbenvensel.fogh@gmail.com>
2023-11-29 20:47:24 +0100

    Change nav to use Mat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { NavigationComponent } from './homepage/Components/navigation/navigation.component';
-import { MovieCarouselComponent } from './homepage/Components/movie-carousel/movie-carousel.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MovieCarouselComponent } from './components/movie-carousel/movie-carousel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSelectModule} from "@angular/material/select";
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatMenuModule} from "@angular/material/menu";
 
 @NgModule({
   declarations: [
@@ -13,12 +23,22 @@ import { MovieCarouselComponent } from './homepage/Components/movie-carousel/mov
     RegisterComponent,
     HomepageComponent,
     NavigationComponent,
-    MovieCarouselComponent
+    MovieCarouselComponent,
+    SearchBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatMenuModule
   ],
   providers: [],
-  bootstrap: [AppComponent,HomepageComponent, NavigationComponent, MovieCarouselComponent, RegisterComponent]
+  bootstrap: [AppComponent,HomepageComponent, NavigationComponent, MovieCarouselComponent, RegisterComponent, SearchBarComponent, HomepageComponent]
 })
 export class AppModule { }
