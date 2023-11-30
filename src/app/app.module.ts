6230bac0be66fdc95280a7102134ffdf1626b2ad
Esben Fogh <esbenvensel.fogh@gmail.com>
2023-11-30 10:50:15 +0100

    Update nav and search-bar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
 import { MovieCarouselComponent } from './components/movie-carousel/movie-carousel.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
@@ -13,18 +12,20 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatSelectModule} from "@angular/material/select";
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatMenuModule} from "@angular/material/menu";
+import {MatCardModule} from "@angular/material/card";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     HomepageComponent,
-    NavigationComponent,
     MovieCarouselComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -36,9 +37,10 @@ import {MatMenuModule} from "@angular/material/menu";
     MatButtonModule,
     MatSelectModule,
     MatGridListModule,
-    MatMenuModule
+    MatMenuModule,
+    MatCardModule
   ],
   providers: [],
-  bootstrap: [AppComponent,HomepageComponent, NavigationComponent, MovieCarouselComponent, RegisterComponent, SearchBarComponent, HomepageComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
