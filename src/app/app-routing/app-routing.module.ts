bb527a664d21acf7d7dc526001290609ec5b93ad
Esben Fogh <esbenvensel.fogh@gmail.com>
2023-12-01 01:15:48 +0100

    Adding remaining pages and update app-module to avoid declaring all views twice

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
-import {RegisterComponent} from "../register/register.component";
-import {MovieDetailsComponent} from "../movie-details/movie-details.component";
-import {HomepageComponent} from "../homepage/homepage.component";
+import {RegisterComponent} from "../views/register/register.component";
+import {MovieDetailsComponent} from "../views/movie-details/movie-details.component";
+import {HomepageComponent} from "../views/homepage/homepage.component";
+import {LoginComponent} from "../views/login/login.component";
+import {ProfileComponent} from "../views/profile/profile.component";
+import {MovieItemComponent} from "../views/movie-item/movie-item.component";
+import {FriendItemComponent} from "../views/friend-item/friend-item.component";
+import {AccountSettingsComponent} from "../views/account-settings/account-settings.component";
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent},
-  { path: 'movie-details', component: MovieDetailsComponent},
-  { path: 'home', component: HomepageComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home',             component: HomepageComponent},
+  { path: 'register',         component: RegisterComponent},
+  { path: 'login',            component: LoginComponent},
+  { path: 'profile',          component: ProfileComponent},
+  { path: 'account-settings', component: AccountSettingsComponent},
+  { path: 'friend-item',      component: FriendItemComponent},
+  { path: 'movie-item',       component: MovieItemComponent},
+  { path: 'movie-details',    component: MovieDetailsComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
@@ -16,4 +26,12 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [RegisterComponent, MovieDetailsComponent];
+export const routingComponents = [
+  HomepageComponent,
+  RegisterComponent,
+  LoginComponent,
+  ProfileComponent,
+  AccountSettingsComponent,
+  FriendItemComponent,
+  MovieItemComponent,
+  MovieDetailsComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {HomepageComponent} from './homepage/homepage.component';
-import {NavbarComponent} from './navbar/navbar.component';
-import {SearchBarComponent} from './search-bar/search-bar.component';
+import {HomepageComponent} from './views/homepage/homepage.component';
+import {SearchBarComponent} from './components/search-bar/search-bar.component';
 import {LoginComponent} from './views/login/login.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
@@ -14,7 +13,7 @@ import {ProfileComponent} from './views/profile/profile.component';
 import {MatTabsModule} from "@angular/material/tabs";
 import {MovieItemComponent} from './views/movie-item/movie-item.component';
 import {MatCardModule} from "@angular/material/card";
-import {CommonModule} from "@angular/common";
+import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {FriendItemComponent} from './views/friend-item/friend-item.component';
 import {AccountSettingsComponent} from './views/account-settings/account-settings.component';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
@@ -22,14 +21,14 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatSelectModule} from "@angular/material/select";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatMenuModule} from "@angular/material/menu";
-import {MovieCarouselComponent} from "./homepage/movie-carousel/movie-carousel.component";
-import { NewsCardComponent } from './homepage/news-card/news-card.component';
+import {MovieCarouselComponent} from "./components/movie-carousel/movie-carousel.component";
+import { NewsCardComponent } from './components/news-card/news-card.component';
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {AppRoutingModule, routingComponents} from "./app-routing/app-routing.module";
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import {FlexLayoutModule} from "@angular/flex-layout";
-import { SidenavListComponent } from './sidenav-list/sidenav-list.component';
+import { SidenavListComponent } from './components/sidenav-list/sidenav-list.component';
 import {MatListModule} from "@angular/material/list";
 
 
@@ -37,15 +36,8 @@ import {MatListModule} from "@angular/material/list";
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
     MovieCarouselComponent,
     SearchBarComponent,
-    NavbarComponent,
-    LoginComponent,
-    ProfileComponent,
-    MovieItemComponent,
-    FriendItemComponent,
-    AccountSettingsComponent,
     NewsCardComponent,
     routingComponents,
     HeaderComponent,
@@ -70,7 +62,8 @@ import {MatListModule} from "@angular/material/list";
     MatSidenavModule,
     AppRoutingModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
