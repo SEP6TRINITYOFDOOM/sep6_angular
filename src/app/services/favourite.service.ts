b39f43a51b122280f18d3201ab413c0a70e9d876
Adam Arasimowicz <75214235+arashimoya@users.noreply.github.com>
2023-12-14 13:58:27 +0100

    favourites

---
+++ src/app/services/favourite.service.ts
@@ -0,0 +1,31 @@
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {Movie} from "./Movie DTO/Movie";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavouriteService {
+
+  private url: string = 'http://localhost:8080/api/favourites';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public addToFavourites(userId: number | null, movieId: number, position: number): Observable<any> {
+    const body = {
+      userId: userId,
+      movieId: movieId,
+      position: position
+    }
+
+    return this.http.post(this.url, body)
+  }
+
+  public getFavourites(userId: number | null): Observable<Movie[]> {
+    return this.http.get<any>(this.url + '?userId=' + userId)
+  }
+
+
+}
--- src/app/views/homepage/homepage.component.ts
+++ src/app/views/homepage/homepage.component.ts
@@ -1,7 +1,4 @@
-import {Component, Input} from '@angular/core';
-import {MovieItemComponent} from "../movie-item/movie-item.component";
-import {FriendItemComponent} from "../friend-item/friend-item.component";
-import {HttpClient} from "@angular/common/http";
+import {Component} from '@angular/core';
 import {MoviesService} from "../../services/movies.service";
 import {ActorsService} from "../../services/actors.service";
 import {Movie} from "../../services/Movie DTO/Movie";
